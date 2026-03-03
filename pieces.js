7546040865c81afeb50d9c989f0a590d71cc7498
Elodie CHANCEREL <elodie.chancerel.44@gmail.com>
2026-03-03 16:35:22 +0100

    exo P3_1

--- pieces.js
+++ pieces.js
@@ -1,3 +1,5 @@
+import { ajoutListenersAvis } from "./avis.js";
+
 // Récupération des pièces depuis le fichier JSON
 const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
@@ -48,6 +50,7 @@ function genererPieces(pieces){
         //Code aJouté en P3_1 bouton avis
         pieceElement.appendChild(avisBouton);
     }
+    ajoutListenersAvis()
 }
 
 genererPieces(pieces)
